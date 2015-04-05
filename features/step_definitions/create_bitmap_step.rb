1127581a44cf9d1f739fd0e143f43b06281c97ee
Diego <diego@guiasrails.es>
2015-04-05 16:45:54 +0200

    change the scenario because in fact the output table should not be shown

--- features/step_definitions/create_bitmap_step.rb
+++ features/step_definitions/create_bitmap_step.rb
@@ -1,3 +1,11 @@
+Given(/^a bitmap created$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^The user type "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
 Then(/^this bitmap should be created$/) do |table|
   # table is a Cucumber::Core::Ast::DataTable
   @bitmap_image = table.raw
